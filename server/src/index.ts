2db84b8868f9944fa5628034139f94bc3cb55654
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-04-17 13:27:40 +0530

    basic server setup

---
+++ server/src/index.ts
@@ -0,0 +1,16 @@
+import express, { Request, Response } from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Hello World!');
+  });
+  
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
+  });
