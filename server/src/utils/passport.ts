df24e62a92e18e823e09fa0eb69b10865b979e31
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-04-18 00:27:42 +0530

    add logic for login passport session

---
+++ server/src/utils/passport.ts
@@ -0,0 +1,24 @@
+import passport from 'passport';
+import { Strategy as GoogleStrategy, Profile, VerifyCallback } from 'passport-google-oauth20';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// console.log(process.env.GOOGLE_CLIENT_ID, process.env.GOOGLE_CLIENT_SECRET, process.env.GOOGLE_CALLBACK_URL);
+
+passport.use("google", new GoogleStrategy({
+    clientID: process.env.GOOGLE_CLIENT_ID || "",
+    clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    callbackURL: process.env.GOOGLE_CALLBACK_URL || "",
+  }, async(accessToken : string, refreshToken: string, profile: Profile, cb : VerifyCallback) => {
+    console.log(profile); 
+    cb(null, profile);
+}));
+
+passport.serializeUser((user: Express.User, cb: (error: any, user?: Express.User | false) => void) => {
+    cb(null, user);
+});
+
+passport.deserializeUser((obj: any, cb: (error: any, user?: Express.User | false) => void) => {
+    cb(null, obj);
+});
