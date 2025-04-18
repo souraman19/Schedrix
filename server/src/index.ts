028ce1e232f482b2da4be4b3cb4f377c522c907a
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-04-19 01:17:47 +0530

    Use mongo-store for making session persistant for server restart issue

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,13 +4,22 @@ import session from 'express-session';
 import passport from 'passport';
 import './utils/passport';
 import authRoutes from './routes/auth.routes';
+import taskRoutes from './routes/tasks.routes';
 import cors from 'cors';
 import { connectDB } from "./config/db";
+import { isAuthenticated } from "./middlewares/AuthMiddleWires";
+import MongoStore from 'connect-mongo';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+// For parsing json data
+app.use(express.json());
+
+// For parsing urlencoded data
+app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
   origin: 'http://localhost:3000',
@@ -22,6 +31,10 @@ app.use(
     secret: process.env.SESSION_SECRET!,
     resave: false,
     saveUninitialized: true,
+    store: MongoStore.create({
+      mongoUrl: process.env.MONGO_URI,
+      ttl: 7 * 24 * 60 * 60, // session expires in 7 days
+    }),
     cookie: {
       httpOnly: true, 
       secure: false, // true in production with HTTPS
@@ -36,7 +49,9 @@ app.use(passport.session());
 
 connectDB();
 
+
 app.use('/api/auth', authRoutes);
+app.use('/api/task', isAuthenticated, taskRoutes);
 
 
 
