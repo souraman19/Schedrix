94c60855256bea7c031341a7c53d28a53d21d3be
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-06-17 14:07:34 +0530

    Add admin sdk to send msg to client

--- server/src/lib/firebase/admin.ts
+++ server/src/lib/firebase/admin.ts
@@ -1,11 +1,10 @@
-// lib/firebase/admin.ts
 import admin from "firebase-admin";
-import serviceAccount from "../../../firebaseServiceAccountKey.json"; // Adjust path
+import serviceAccount from "./firebaseServiceAccountKey.json";
 
 if (!admin.apps.length) {
   admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount),
+    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
   });
-}
+} // Initialize Firebase Admin SDK only if it hasn't been initialized yet
 
 export { admin };
