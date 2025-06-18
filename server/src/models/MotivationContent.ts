a8294ef5acb9046ea00d4e187c87fb12e56c5713
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-06-18 13:44:12 +0530

    Add Schema for Motivation Content

---
+++ server/src/models/MotivationContent.ts
@@ -0,0 +1,50 @@
+import mongoose from "mongoose";
+import { Document, Schema, model, Types } from "mongoose";
+
+export interface IMotivationContent extends Document {
+  type: string;  //Content type: quote/video/etc
+  content: string;  //Actual quote or message
+  link?: string;  //YouTube or external content link
+  tags: [string];
+  source: string;
+  fetchedAt: Date;
+  contentHash: string; //Prevents duplicate entries
+}
+
+const motivationContentSchema = new Schema<IMotivationContent>({
+  type: {
+    type: String,
+    required: true,
+    enum: ["quote", "video", "affirmation"],
+  },
+  content: {
+    type: String,
+    required: true,
+  },
+  link: {
+    type: String,
+    default: null,
+  },
+  tags: {
+    type: [String],
+    default: []
+  },
+  source: {
+    type: String,
+    required: true
+  },
+  fetchedAt:{
+    type: Date,
+    default: Date.now
+  },
+  contentHash:{
+    type: String,
+    required: true,
+    unique: true
+  },
+});
+
+export const MotivationContent = model<IMotivationContent>(
+  "MotivationContent",
+  motivationContentSchema
+);
