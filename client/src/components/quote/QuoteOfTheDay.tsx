8aa5f3ecebf7f30292481a73333947d3f15e27e9
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-06-21 16:48:48 +0530

    Get the QOTD from backend and show it

--- client/src/components/quote/QuoteOfTheDay.tsx
+++ client/src/components/quote/QuoteOfTheDay.tsx
@@ -1,31 +1,44 @@
 import { GET_QUOTE_OF_THE_DAY_ROUTE } from "@/lib/apiRoutes";
 import { cookies } from "next/headers";
 
-export default async function QuoteOfTheDay({mindStatus}: {mindStatus: string | null}) {
-    try{
-        const cookieStore = await cookies();
-        const sessionCookie = cookieStore.get("connect.sid");
-        const response = await fetch(`${GET_QUOTE_OF_THE_DAY_ROUTE}?mindStatus=${mindStatus}`, {
-            method: "GET",
-            headers: {
-                Cookie: `${sessionCookie?.name}=${sessionCookie?.value}`,
-            },
-            cache: "no-store",
-        });
-        if(response.status === 200){
-            const result = await response.json();
-            const quote = result.quote;
-            console.log("Quote of the Day fetched successfully: ", quote);
-        }
-    }catch(err){
-        console.error("Error fetching task details:", err);
-    }
+export default async function QuoteOfTheDay({
+  mindStatus,
+}: {
+  mindStatus: string | null;
+}) {
+  let QOTD: string | null = null;
 
-    return (
-        <div className="flex items-center justify-center h-screen">
-            <div >
-                {mindStatus}
-            </div>
-        </div>
+  try {
+    const cookieStore = await cookies();
+    const sessionCookie = cookieStore.get("connect.sid");
+    const response = await fetch(
+      `${GET_QUOTE_OF_THE_DAY_ROUTE}?mindStatus=${mindStatus}`,
+      {
+        method: "GET",
+        headers: {
+          Cookie: `${sessionCookie?.name}=${sessionCookie?.value}`,
+        },
+        cache: "no-store",
+      }
     );
+
+    if (response.status === 200) {
+      const result = await response.json();
+      QOTD = result.quote;
+    //   console.log("Quote of the Day fetched successfully: ", quote);
+    }
+  } catch (err) {
+    console.error("Error fetching task details:", err);
+  }
+
+  return (
+    <div className="flex items-center justify-center h-screen">
+      {QOTD && (
+        <div>
+          <div>{QOTD.content}</div>
+          <div>{QOTD.author}</div>
+        </div>
+      )}
+    </div>
+  );
 }
