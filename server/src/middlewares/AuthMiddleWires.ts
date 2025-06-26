307baee344388a5f51c145a7d9c5036fc5dd00d1
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-06-26 18:38:18 +0530

    ..

--- server/src/middlewares/AuthMiddleWires.ts
+++ server/src/middlewares/AuthMiddleWires.ts
@@ -3,6 +3,7 @@ export const isAuthenticated = (req: any, res: any, next: any) => {
         console.log("User is authenticated");
         return next(); 
     }
-    console.log("User is not authenticated", req);
+    console.log("User is not  req session", req.session);
+    console.log("User is not authenticated req isauth", req.isAuthenticated);
     res.status(401).json({ message: 'User not authenticated' }); 
 }
