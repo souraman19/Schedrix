5c8bc40518288743bc6d2df2cc170777d8fd6bd2
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-06-26 21:36:11 +0530

    convert firebaseServiceAccountKeytoBase64 and use as environment variable in render

--- server/src/lib/firebase/admin.ts
+++ server/src/lib/firebase/admin.ts
@@ -1,5 +1,15 @@
 import admin from "firebase-admin";
-import serviceAccount from "./firebaseServiceAccountKey.json";
+// import serviceAccount from "./firebaseServiceAccountKey.json";
+
+const base64 = process.env.FIREBASE_SERVICE_ACCOUNT_BASE64;
+
+if (!base64) {
+  throw new Error("Missing Firebase service account in env");
+}
+
+const serviceAccount = JSON.parse(
+  Buffer.from(base64, "base64").toString("utf8")
+);
 
 if (!admin.apps.length) {
   admin.initializeApp({
