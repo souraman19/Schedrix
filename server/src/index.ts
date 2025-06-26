a72b25aabeef50f75d9d85f1425eb593ada8917f
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-06-26 17:28:39 +0530

    Add console.log msg

--- server/src/index.ts
+++ server/src/index.ts
@@ -19,7 +19,7 @@ import path from "path";
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000 || 7000 || 11000;
 
 // For parsing json data
 app.use(express.json());
--- server/src/middlewares/AuthMiddleWires.ts
+++ server/src/middlewares/AuthMiddleWires.ts
@@ -3,5 +3,6 @@ export const isAuthenticated = (req: any, res: any, next: any) => {
         console.log("User is authenticated");
         return next(); 
     }
+    console.log("User is not authenticated");
     res.status(401).json({ message: 'User not authenticated' }); 
 }
