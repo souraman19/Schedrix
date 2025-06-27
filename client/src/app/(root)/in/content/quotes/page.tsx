7a2c29af6cd24c13be663036519ea95e21c226e8
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-06-27 23:00:51 +0530

    Improve style

--- client/src/app/(root)/in/content/quotes/page.tsx
+++ client/src/app/(root)/in/content/quotes/page.tsx
@@ -31,12 +31,12 @@ export default function ContentHomePage() {
 
   return (
     <div className="min-h-screen px-4 pt-0 bg-black text-white">
-            <h1 className="text-3xl font-bold text-center mb-8 bg-gradient-to-r from-green-400 to-lime-300 bg-clip-text text-transparent">
-            Quote of the Day <span className="text-white">✨</span>
-          </h1>
       {!mindStatus ? (
         <div className="mb-10 mt-0 flex items-center justify-center min-h-screen px-4 py-10 bg-gradient-to-br from-black via-[#0d0d0d] to-[#1a1a1a] text-white">
           <div className="flex flex-col items-center justify-center w-full max-w-3xl animate-pulse">
+            <h1 className="text-3xl font-bold text-center mb-8 bg-gradient-to-r from-green-400 to-lime-300 bg-clip-text text-transparent">
+              Quote of the Day
+            </h1>
 
             <div className="relative bg-gradient-to-br py-5 px-10 from-[#0f0f0f] to-[#1c1c1c] p-4 rounded-2xl max-w-sm w-full border border-[#2e2e2e] shadow-[0_0_20px_#00c85333]">
               {/* Top Glow Accent */}
