9cc64fc824412f30ce2f6b6226078b0a2ca82795
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-06-28 00:30:17 +0530

    add background service

---
+++ server/src/backgroundService.ts
@@ -0,0 +1,5 @@
+import "./tasks/cronJob";
+import "./tasks/CheckMissedCron";
+import "./lib/queues/reminderWorker";
+import "./lib/queues/motivationalVideoFetchWorker";
+import "./lib/queues/quoteFetchWorker";
