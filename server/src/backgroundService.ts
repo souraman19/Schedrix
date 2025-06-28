2519214172c4f21f10475e774de21000553fe9f2
Sourajit Mandal <140224488+souraman19@users.noreply.github.com>
2025-06-28 08:25:16 +0530

    Unable to use paid background worker in render do goes back to web service method instead

--- server/src/backgroundService.ts
+++ server/src/backgroundService.ts
@@ -1,4 +1,9 @@
 import { connectDB } from "./config/db";
+import express, {Request, Response} from "express";
+
+const app = express();
+const port = process.env.PORT || 8000;
+
 
 async function initBackgroundServices() {
   await connectDB();
@@ -8,11 +13,20 @@ async function initBackgroundServices() {
   await import("./lib/queues/reminderWorker");
   await import("./lib/queues/motivationalVideoFetchWorker");
   await import("./lib/queues/quoteFetchWorker");
-
+  
   console.log("🔄 Background services initialized");
 }
 
 initBackgroundServices().catch((err) => {
-  console.error("❌ Failed to initialize services:", err);
-  process.exit(1);
+    console.error("❌ Failed to initialize services:", err);
+    process.exit(1);
+});
+
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Background services are running");
+});
+
+app.listen(port, () => {
+  console.log(`Background service server is running on http://localhost:${port}`);
 });
